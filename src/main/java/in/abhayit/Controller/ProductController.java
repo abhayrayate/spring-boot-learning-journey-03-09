2a595a528557f47b2b2bcf7831674c312657de35
Abhay Rayate <you@example.com>
2025-09-03 16:58:45 +0530

    Initial commit

---
+++ src/main/java/in/abhayit/Controller/ProductController.java
@@ -0,0 +1,29 @@
+package in.abhayit.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import in.abhayit.binding.Product;
+
+@Controller
+public class ProductController {
+
+	//method to display form
+	@GetMapping("/")
+	public String loadForm(Model model)
+	{
+		model.addAttribute("product", new Product());
+		return "index";
+	}
+	
+	//method to handle form Submission
+	@PostMapping("/product")
+	public String handleSubmitbtn(Product p,Model model)
+	{
+		System.out.println(p);
+		model.addAttribute("msg","Product Saved");
+		return"index";
+	}
+}
---
+++ src/main/java/in/abhayit/binding/Product.java
@@ -0,0 +1,32 @@
+package in.abhayit.binding;
+
+public class Product {
+
+	private Integer pid;
+	private String pname;
+	private String price;
+	public Integer getPid() {
+		return pid;
+	}
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Product [pid=" + pid + ", pname=" + pname + ", price=" + price + "]";
+	}
+	
+	
+}
